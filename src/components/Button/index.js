859b05ec1177c307704092532620d66954583f25
BrendaLorencon <brenda.silva.avatar@gmail.com>
2021-05-08 14:15:44 -0300

    Correção do disabled

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,6 +2,6 @@ import './button.scss';
 
 export default function Button(props){ 
   return (
-    <button className={ `button ${props.className}` } onClick={props.onClick}>{props.title}</button>
+    <button className={ `button ${props.className}` } disabled={props.disabled} onClick={props.onClick}>{props.title}</button>
   )
 } 
