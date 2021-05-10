464220cf3087775d0e40d8fec04ac83846f77e17
BrendaLorencon <brenda.silva.avatar@gmail.com>
2021-05-09 22:14:09 -0300

    Deletado algumas configurações de router

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import { Modal } from "./components/Modal";
 import { tableData } from "./AppData";
 import ModalHiguerIncome from "./components/ModalHiguerIncome";
 import ModalValuesStates from "./components/ModalValuesStates";
-import Router from "./Router";
 
 function App() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
@@ -55,7 +54,6 @@ function App() {
 
   return (
     <>
-    <Router />
       <Header />
       <div className="buttons-action">
         <Button title="Incluir" onClick={() => setModalIsOpen(true)} />
